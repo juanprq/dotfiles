8c89d45e22fb3061be4a6fbd57b16b27eacb476c
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2024-02-19 12:06:28 -0500

    Adds more plugins configurations

--- .config/nvim/lua/juanprq/plugins/null-ls.lua
+++ .config/nvim/lua/juanprq/plugins/null-ls.lua
@@ -1,13 +1,13 @@
 return {
-  'jose-elias-alvarez/null-ls.nvim',
-  config = function ()
+  'nvimtools/none-ls.nvim',
+  config = function()
     local null_ls = require('null-ls');
-      null_ls.setup({
+    null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.eslint,
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.completion.spell,
       },
     })
