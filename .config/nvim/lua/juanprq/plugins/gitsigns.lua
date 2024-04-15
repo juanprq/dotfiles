e335c1abbae8743669cbf729f1f84605fa6def3a
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2024-04-15 14:19:10 -0500

    adds configuration improvements for neovim

--- .config/nvim/lua/juanprq/plugins/gitsigns.lua
+++ .config/nvim/lua/juanprq/plugins/gitsigns.lua
@@ -1,7 +1,13 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({ signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    }})
 
     vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
     vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
--- .config/nvim/lua/juanprq/plugins/null-ls.lua
+++ .config/nvim/lua/juanprq/plugins/null-ls.lua
@@ -6,8 +6,8 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.code_actions.eslint_d,
+        -- null_ls.builtins.diagnostics.eslint_d,
+        -- null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.completion.spell,
       },
     })
