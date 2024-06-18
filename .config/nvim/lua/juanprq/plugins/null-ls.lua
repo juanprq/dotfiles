b944d9b9212f9d9561436a696ab0440f8dfa4b14
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2024-06-18 12:57:31 -0500

    Fix none-ls configuration

--- .config/nvim/lua/juanprq/plugins/null-ls.lua
+++ .config/nvim/lua/juanprq/plugins/null-ls.lua
@@ -1,21 +1,25 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.eslint_d,
-        require("none-ls.diagnostics.eslint"),
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
-		vim.keymap.set("n", "<leader>p", ":Format<cr>")
-	end,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.code_actions.refactoring,
+
+        require("none-ls.formatting.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
+        require("none-ls.diagnostics.eslint_d"),
+      },
+    })
+
+    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
+    vim.keymap.set("n", "<leader>p", ":Format<cr>")
+  end,
 }
